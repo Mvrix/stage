a41eff9867d0c0221e86b4dfb36a2e33d8a5465d
Mariu <oi_mario@live.com>
2022-11-11 15:06:00 -0300

    js

---
+++ JAVASCRIPT DOM/js/main.js
@@ -0,0 +1,14 @@
+const robotron = document.querySelector('#robotron');
+
+robotron.addEventListener('click', (evento) => {
+    console.log(evento);
+}
+
+);
+
+function dizOi(nome){
+    console.log(nome);
+    console.log('bem-vindo ao robotron 2000');
+}
+
+dizOi('pedro')
