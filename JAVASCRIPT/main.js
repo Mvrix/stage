078296f9d353a9055ab28969c61375104deb027b
Mariu <oi_mario@live.com>
2022-11-11 11:58:08 -0300

    add sound

---
+++ JAVASCRIPT/main.js
@@ -0,0 +1,17 @@
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//let contador = 0;
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+}
