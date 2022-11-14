972bb4eec4d4f547be893abb2c7551a03f4e6903
Mariu <oi_mario@live.com>
2022-11-14 16:25:42 -0300

    array_seach
    verificações de dados string

--- PHP MANIPULANDO ARRAYS/is_array_e_array_is_list.php
+++ PHP MANIPULANDO ARRAYS/is_array_e_array_is_list.php
@@ -10,14 +10,15 @@ $notas = [
 asort($notas);
 var_dump($notas);
 
-if (is_array($notas)){
+if (is_array($notas)) {
     echo "Isso é uma Array" . PHP_EOL;
 }
 
 var_dump(array_is_list($notas)); //verificação de tipo de array - se é possivel usar for para percorrer array
 
 echo "Mario fez a prova: " . PHP_EOL;
-var_dump(array_key_exists('Mario' , $notas)); //verifica se existe na array - melhor para uso de dados de fora (banco/url)
+var_dump(array_key_exists('Mario', $notas)); //verifica se existe na array - melhor para uso de dados de fora (banco/url)
+
 
 echo "Mario fez a prova: " . PHP_EOL;
 var_dump(isset($notas['Mario'])); //verifica se existe na array e se é diferente de null
@@ -25,3 +26,7 @@ var_dump(isset($notas['Mario'])); //verifica se existe na array e se é diferent
 
 echo "Alguem tirou 10?" . PHP_EOL;
 var_dump(in_array(10, $notas)); //verifica se o dado possui na array
+
+
+echo "Quem tirou 10?" . PHP_EOL;
+echo array_search(10, $notas, true); //verifica o dado dentro da array ;)
