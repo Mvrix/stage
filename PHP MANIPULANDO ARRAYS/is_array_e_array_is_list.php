ce2350dd5bbbb9c105e195e02c3eb445dc4e57ce
Mariu <oi_mario@live.com>
2022-11-14 16:11:16 -0300

    verificações com outros metodos de arrays
    verificação se existe/ se é nulo ou não/ verifica se valor existe dentro da array

--- PHP MANIPULANDO ARRAYS/is_array_e_array_is_list.php
+++ PHP MANIPULANDO ARRAYS/is_array_e_array_is_list.php
@@ -1,7 +1,7 @@
 <?php
 
 $notas = [
-    'Mario' => 1,
+    'Mario' => null,
     'Ana' => 10,
     'Caio' => 5,
     'Luan' => 3
@@ -15,3 +15,11 @@ if (is_array($notas)){
 }
 
 var_dump(array_is_list($notas)); //verificação de tipo de array - se é possivel usar for para percorrer array
+
+echo "Mario fez a prova: " . PHP_EOL;
+var_dump(array_key_exists('Mario' , $notas)); //verifica se existe na array - melhor para uso de dados de fora (banco/url)
+
+echo "Mario fez a prova: " . PHP_EOL;
+var_dump(isset($notas['Mario'])); //verifica se existe na array e se é diferente de null
+
+
