b5a3b8cc0886ea1691dd906870ca275729a98243
Mariu <oi_mario@live.com>
2022-11-14 17:09:19 -0300

    introduction a arrays combine

--- PHP MANIPULANDO ARRAYS/arrays_plus.php
+++ PHP MANIPULANDO ARRAYS/arrays_plus.php
@@ -16,4 +16,7 @@ $notasBimestre2 = [
 
 var_dump(array_diff($notasBimestre1, $notasBimestre2));  //comparação de valores na array - retorna em uma nova array -array_diff
 
-var_dump(array_diff_key($notasBimestre1, $notasBimestre2));
+var_dump(array_diff_key($notasBimestre1, $notasBimestre2)); //comparação de dados na array 1 e 2 - retorna o dado que não está nas array 1 ou 2 - key
+
+var_dump(array_diff_assoc($notasBimestre1, $notasBimestre2)); //comparação de chaves e valores nas 2 arrays e retorna valor que não esta nas duas arrays -assoc - associação
+
