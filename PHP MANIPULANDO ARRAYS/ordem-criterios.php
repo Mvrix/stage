3b6d558b10a2e14911d152f8fe79166d1c816e16
Mariu <oi_mario@live.com>
2022-11-14 15:25:50 -0300

    code reduction
    redução de codigo com o operador <=>

--- PHP MANIPULANDO ARRAYS/ordem-criterios.php
+++ PHP MANIPULANDO ARRAYS/ordem-criterios.php
@@ -17,6 +17,14 @@ $notas = [
 
 function ordenaNotas(array $nota1, array $nota2) : int
 {
+    return $nota2['nota'] <=> $nota1['nota'];
+}
+
+
+/*
+redução de codigo
+
+{
     if ($nota1['nota'] > $nota2['nota']){
         return -1;
     }
@@ -26,6 +34,7 @@ function ordenaNotas(array $nota1, array $nota2) : int
 
         return 0;
 }
+*/
 
 usort($notas , 'ordenaNotas');  // não modificando a variavel
 
