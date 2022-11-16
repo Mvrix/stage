a383d524319cb07541d9a51e062ddfcbd5866b88
Mariu <oi_mario@live.com>
2022-11-16 14:51:07 -0300

    function ob

--- PHP ORIENTADO-OBJETO/src/conta.php
+++ PHP ORIENTADO-OBJETO/src/conta.php
@@ -2,7 +2,17 @@
 
 class Conta
 {
+    public $cpfTitular;
+    public $nomeTitular;
+    public $saldo;
     
+    public function sacar(float $valorASacar) {
+        if ($valorASacar >= $saldo) {
+            echo "Saldo indisponivel";
+        } else {
+            $saldo -= $valorASacar;
+        }
+    }
 }
 
 
