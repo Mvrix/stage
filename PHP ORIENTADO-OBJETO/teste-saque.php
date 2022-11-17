ecf82e8ed0c293750b1153b5ff9f3929c04ed7bc
Mariu <oi_mario@live.com>
2022-11-17 12:26:42 -0300

    Update teste-saque.php

--- PHP ORIENTADO-OBJETO/teste-saque.php
+++ PHP ORIENTADO-OBJETO/teste-saque.php
@@ -8,7 +8,8 @@ use Stage\Banco\Modelo\Conta\{Conta, ContaCorrente, Titular};
 require_once 'autoload.php';
 
 $conta = new ContaCorrente(
-    new Titular("Mario", "123.123.132-12", new endereco("Soro", "wanel", "rua", "9")));
+    new Titular("123.123.132-12"), "Mario" ,
+    new endereco("Soro", "wanel", "rua", "9"));
 
 $conta->deposita(500);
 $conta->saca(100);
